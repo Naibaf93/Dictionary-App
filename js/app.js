e83224d29e6f847dbac6853d0965c666d5894275
Fabián Huertas <miloh9318@hotmail.com>
2026-02-22 20:24:18 -0500

    traspasando las etiquetas del html para generar manipulacion de DOM

--- js/app.js
+++ js/app.js
@@ -6,6 +6,23 @@ const btn = document.getElementById("search-btn");
 btn.addEventListener("click", () => {
     let inputWord = document.getElementById("input-word").value;
     fetch(`${url}${inputWord}`)
-        .then((response) => response.json)
-        .then((data) => console.log(data));
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                <h3>Sample</h3>
+                <button>
+                <i class="fa-solid fa-volume-high"></i>
+                </button>
+            </div>
+            <div class="details">
+                <p>pos</p>
+                <p>/sample/</p>
+            </div>
+            <p class="word-meaning">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, sunt.
+            </p>
+            <p class="word-example">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, illum!</p>`;
+        });
 });
